41ea773e31c4fcddafef70654bc6aec9a99e66bc
Maciej Makowski <maciej.makowski@swmansion.com>
2024-08-21 16:35:49 +0200

    refactor: refactored AudioNode

--- cpp/AudioNode/AudioNodeWrapper.h
+++ cpp/AudioNode/AudioNodeWrapper.h
@@ -17,9 +17,9 @@ namespace audiocontext {
         explicit AudioNodeWrapper(const std::shared_ptr<AudioNode> &node);
         virtual ~AudioNodeWrapper();
 #else
-        public:
-            std::shared_ptr<IOSAudioNode> node_;
-            explicit AudioNodeWrapper() {}
+    public:
+        std::shared_ptr<IOSAudioNode> node_;
+        explicit AudioNodeWrapper() {}
 #endif
     private:
         int numberOfInputs_;
