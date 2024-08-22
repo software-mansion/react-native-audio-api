9d2d136e329b7ec758bfd8179a4fcb773cc014f6
Maciej Makowski <maciej.makowski@swmansion.com>
2024-08-22 10:48:32 +0200

    feat: implemented BiquadFilterNode

---
+++ ios/nodes/BiquadFilterNode/IOSBiquadFilterNode.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#ifdef __OBJC__ // when compiled as Objective-C++
+#import <BiquadFilterNode.h>
+#else // when compiled as C++
+typedef struct objc_object BiquadFilterNode;
+#endif // __OBJC__
+
+#include <string>
+#include "IOSAudioParam.h"
+#include "IOSAudioNode.h"
+#include "IOSAudioContext.h"
+
+namespace audiocontext {
+    class IOSBiquadFilterNode : public IOSAudioNode {
+        public:
+            explicit IOSBiquadFilterNode(std::shared_ptr<IOSAudioContext> context);
+            ~IOSBiquadFilterNode();
+            void setType(const std::string &type) const;
+            std::string getType() const;
+            std::shared_ptr<IOSAudioParam> getFrequencyParam();
+            std::shared_ptr<IOSAudioParam> getDetuneParam();
+            std::shared_ptr<IOSAudioParam> getQParam();
+            std::shared_ptr<IOSAudioParam> getGainParam();
+
+        protected:
+            BiquadFilterNode *biquadFilterNode_;
+    };
+} // namespace audiocontext
