4db447b0d6cfbad4c5bc713cbd4b61a86dc2acd8
Maciej Makowski <maciej.makowski@swmansion.com>
2024-09-06 16:32:44 +0200

    refactor: refactored IOSAudioContext

--- ios/context/IOSAudioContext.h
+++ ios/context/IOSAudioContext.h
@@ -8,16 +8,31 @@ typedef struct objc_object AudioContext;
 
 #include <string>
 #include "IOSAudioContext.h"
+#include "IOSGainNode.h"
+#include "IOSOscillatorNode.h"
+#include "IOSBiquadFilterNode.h"
+#include "IOSStereoPannerNode.h"
+#include "IOSAudioDestinationNode.h"
+#include "IOSAudioBuffer.h"
+#include "IOSAudioBufferSourceNode.h"
 
 namespace audioapi {
 class IOSAudioContext {
  public:
+    AudioContext *audioContext_;
+    
   explicit IOSAudioContext();
   ~IOSAudioContext();
+    
+    std::shared_ptr<IOSAudioDestinationNode> getDestination();
+    std::shared_ptr<IOSOscillatorNode> createOscillator();
+    std::shared_ptr<IOSGainNode> createGain();
+    std::shared_ptr<IOSStereoPannerNode> createStereoPanner();
+    std::shared_ptr<IOSAudioBufferSourceNode> createBufferSource();
+    std::shared_ptr<IOSAudioBuffer> createBuffer(int sampleRate, int length, int numberOfChannels);
   void close();
   double getCurrentTime();
   std::string getState();
   double getSampleRate();
-  AudioContext *audioContext_;
 };
 } // namespace audioapi
