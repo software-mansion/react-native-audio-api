01fbe007d9243a5a6b730f6d4c957ef9061b143f
Maciej Makowski <maciej.makowski@swmansion.com>
2024-09-09 12:20:43 +0200

    feat: implemented AudioBuffer

---
+++ ios/utils/AudioBuffer/IOSAudioBuffer.mm
@@ -0,0 +1,54 @@
+#import "IOSAudioBuffer.h"
+
+namespace audioapi {
+
+IOSAudioBuffer::IOSAudioBuffer(RNAA_AudioBuffer *audioBuffer)
+{
+  audioBuffer_ = audioBuffer;
+}
+
+IOSAudioBuffer::~IOSAudioBuffer()
+{
+  [audioBuffer_ cleanup];
+  audioBuffer_ = nullptr;
+}
+
+int IOSAudioBuffer::getSampleRate()
+{
+  return audioBuffer_.sampleRate;
+}
+
+int IOSAudioBuffer::getLength()
+{
+  return audioBuffer_.length;
+}
+
+int IOSAudioBuffer::getNumberOfChannels()
+{
+  return audioBuffer_.numberOfChannels;
+}
+
+float IOSAudioBuffer::getDuration()
+{
+  return audioBuffer_.duration;
+}
+
+float *IOSAudioBuffer::getChannelData(int channel)
+{
+  @try {
+    return [audioBuffer_ getChannelDataForChannel:channel];
+  } @catch (NSException *exception) {
+    NSLog(@"Exception: %@", exception);
+  }
+}
+
+void IOSAudioBuffer::setChannelData(int channel, float *data, int length)
+{
+  @try {
+    [audioBuffer_ setChannelData:channel data:data length:length];
+  } @catch (NSException *exception) {
+    NSLog(@"Exception: %@", exception);
+  }
+}
+
+} // namespace audioapi
