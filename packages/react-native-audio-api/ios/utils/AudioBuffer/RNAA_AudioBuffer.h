3c496177f6db1776c712cc52e6a8f553402f3fde
Maciej Makowski <maciej.makowski@swmansion.com>
2024-09-19 11:54:15 +0200

    feat: implemented logic of AudioBuffer on IOS

--- packages/react-native-audio-api/ios/utils/AudioBuffer/RNAA_AudioBuffer.h
+++ packages/react-native-audio-api/ios/utils/AudioBuffer/RNAA_AudioBuffer.h
@@ -17,8 +17,8 @@
 
 - (void)setChannelData:(int)channel data:(float *)data length:(int)length;
 
-- (RNAA_AudioBuffer *)copyBuffer;
-
-- (RNAA_AudioBuffer *)mixWithOutputNumberOfChannels:(int)outputNumberOfChannels;
+//- (RNAA_AudioBuffer *)copyBuffer;
+//
+//- (RNAA_AudioBuffer *)mixWithOutputNumberOfChannels:(int)outputNumberOfChannels;
 
 @end
