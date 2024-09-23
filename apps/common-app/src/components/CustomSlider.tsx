0a7847193fb893a3c292da3d158ba5a6d8104f5b
Maciej Makowski <maciej.makowski@swmansion.com>
2024-09-23 11:54:34 +0200

    feat: implemented custom slider

--- apps/common-app/src/components/CustomSlider.tsx
+++ apps/common-app/src/components/CustomSlider.tsx
@@ -1,100 +1,106 @@
-import { FC } from 'react';
-import { StyleSheet, View } from 'react-native';
+import { FC, useState, useEffect, useCallback } from 'react';
+import { StyleSheet, View, StyleProp, ViewStyle } from 'react-native';
 import { Gesture, GestureDetector } from 'react-native-gesture-handler';
 import Animated, {
-  Extrapolation,
   clamp,
-  interpolate,
-  measure,
   useAnimatedRef,
   useAnimatedStyle,
   useSharedValue,
-  withSpring,
+  runOnJS,
 } from 'react-native-reanimated';
 
 import { colorShades, layout } from '../styles';
-import Container from './Container';
 
 const hitSlop = { top: 20, left: 20, right: 20, bottom: 20 };
 
 interface CustomSliderProps {
-  value?: number;
-  onValueChange?: (value: number) => void;
-  minimumValue?: number;
-  maximumValue?: number;
-  step?: number;
+  value: number;
+  onValueChange: (value: number) => void;
+  minimumValue: number;
+  maximumValue: number;
+  step: number;
+  style?: StyleProp<ViewStyle>;
 }
 
 const CustomSlider: FC<CustomSliderProps> = (props) => {
-  const { value, onValueChange, minimumValue, maximumValue, step } = props;
+  const { value, onValueChange, minimumValue, maximumValue, step, style } =
+    props;
   const x = useSharedValue(0);
-  const knobScale = useSharedValue(1);
-
-  const tapGesture = Gesture.Tap()
-    .maxDuration(100000)
-    .onStart(() => {
-      knobScale.value = withSpring(1);
-    })
-    .onEnd(() => {
-      knobScale.value = withSpring(0);
-    });
-
   const aRef = useAnimatedRef<View>();
+  const [width, setWidth] = useState<number>(300);
+
+  const convertXToValue = useCallback(
+    (xValue: number) => {
+      const fraction = xValue / width;
+      const newValue = minimumValue + fraction * (maximumValue - minimumValue);
+      runOnJS(onValueChange)(Math.round(newValue / step) * step);
+    },
+    [width, minimumValue, maximumValue, step, onValueChange]
+  );
+
+  const convertValueToX = useCallback(
+    (newValue: number) => {
+      const fraction =
+        (newValue - minimumValue) / (maximumValue - minimumValue);
+      x.value = fraction * width;
+    },
+    [width, minimumValue, maximumValue, x]
+  );
 
   const panGesture = Gesture.Pan()
     .averageTouches(true)
     .onChange((ev) => {
-      const size = measure(aRef);
-      x.value = clamp((x.value += ev.changeX), 0, size.width);
+      x.value = clamp((x.value += ev.changeX), 0, width);
+      runOnJS(convertXToValue)(x.value);
     })
     .onEnd(() => {
-      knobScale.value = withSpring(1);
+      runOnJS(convertXToValue)(x.value);
     });
-  const gestures = Gesture.Simultaneous(tapGesture, panGesture);
+
   const animatedStyle = useAnimatedStyle(() => {
     return {
-      borderWidth: interpolate(
-        knobScale.value,
-        [0, 1],
-        [layout.knobSize / 2, 2],
-        Extrapolation.CLAMP
-      ),
       transform: [
         {
           translateX: x.value,
-        },
-        {
-          scale: knobScale.value,
         },
       ],
     };
   });
 
+  useEffect(() => {
+    if (style && 'width' in style) {
+      setWidth(style.width as number);
+    }
+
+    runOnJS(convertValueToX)(value);
+  }, [style, value, minimumValue, maximumValue, convertValueToX]);
+
   return (
-    <Container centered={true}>
-      <GestureDetector gesture={gestures}>
+    <View style={[styles.default, style]}>
+      <GestureDetector gesture={panGesture}>
         <View ref={aRef} style={styles.slider} hitSlop={hitSlop}>
           <Animated.View style={[styles.progress, { width: x }]} />
           <Animated.View style={[styles.knob, animatedStyle]} />
         </View>
       </GestureDetector>
-    </Container>
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
+  default: {
+    width: 300,
+  },
   knob: {
     width: layout.knobSize,
     height: layout.knobSize,
     borderRadius: layout.knobSize / 2,
-    backgroundColor: colorShades.white.base,
     borderWidth: layout.knobSize / 2,
     borderColor: colorShades.blue.base,
     position: 'absolute',
     left: -layout.knobSize / 2,
   },
   slider: {
-    width: '80%',
     backgroundColor: colorShades.blue.light,
     height: 5,
     justifyContent: 'center',
