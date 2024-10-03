e672361d7e06ee5b10d21b7fff26df3214895cf4
Maciej Makowski <maciej.makowski@swmansion.com>
2024-10-03 12:10:21 +0200

    feat: implemented AudioBuffer

--- packages/react-native-audio-api/android/src/main/cpp/AudioBuffer/AudioBuffer.cpp
+++ packages/react-native-audio-api/android/src/main/cpp/AudioBuffer/AudioBuffer.cpp
@@ -5,6 +5,50 @@ namespace audioapi {
 AudioBuffer::AudioBuffer(int numberOfChannels, int length, int sampleRate)
     : numberOfChannels_(numberOfChannels),
       length_(length),
-      sampleRate_(sampleRate) {}
+      sampleRate_(sampleRate),
+      duration_(static_cast<double>(length) / sampleRate),
+      channels_(numberOfChannels, std::vector<float>(length, 0.0f)) {}
 
+int AudioBuffer::getNumberOfChannels() const {
+    return numberOfChannels_;
+}
+
+int AudioBuffer::getLength() const {
+    return length_;
+}
+
+int AudioBuffer::getSampleRate() const {
+    return sampleRate_;
+}
+
+double AudioBuffer::getDuration() const {
+    return duration_;
+}
+
+float *AudioBuffer::getChannelData(int channel) const {
+    if (channel < 0 || channel >= numberOfChannels_) {
+        throw std::invalid_argument("Invalid channel number");
+    }
+
+    auto channelData = (float *)malloc(length_ * sizeof(float));
+    for (int i = 0; i < length_; i++) {
+        channelData[i] = channels_[channel][i];
+    }
+
+    return channelData;
+}
+
+void AudioBuffer::setChannelData(int channel, const float *data, int length) {
+    if (channel < 0 || channel >= numberOfChannels_) {
+        throw std::invalid_argument("Invalid channel number");
+    }
+
+    if (length != length_) {
+        throw std::invalid_argument("Invalid data length");
+    }
+
+    for (int i = 0; i < length_; i++) {
+        channels_[channel][i] = data[i];
+    }
+}
 } // namespace audioapi
--- packages/react-native-audio-api/android/src/main/cpp/AudioBuffer/AudioBuffer.h
+++ packages/react-native-audio-api/android/src/main/cpp/AudioBuffer/AudioBuffer.h
@@ -1,8 +1,5 @@
 #pragma once
 
-#include <fbjni/fbjni.h>
-#include <react/jni/CxxModuleWrapper.h>
-#include <react/jni/JMessageQueueThread.h>
 #include <memory>
 #include <string>
 #include <vector>
@@ -15,10 +12,19 @@ class AudioBuffer {
  public:
   explicit AudioBuffer(int numberOfChannels, int length, int sampleRate);
 
+  int getNumberOfChannels() const;
+    int getLength() const;
+    int getSampleRate() const;
+    double getDuration() const;
+    float *getChannelData(int channel) const;
+    void setChannelData(int channel, const float *data, int length);
+
  private:
   int numberOfChannels_;
   int length_;
   int sampleRate_;
+  double duration_;
+  std::vector<std::vector<float>> channels_;
 };
 
 } // namespace audioapi
