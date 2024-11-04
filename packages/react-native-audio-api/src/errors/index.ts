724d440b0791a4e4b26a5ff95566bf5cd6d6b1aa
Maciej Makowski <maciej.makowski@swmansion.com>
2024-11-04 16:34:40 +0100

    feat: added new error type

--- packages/react-native-audio-api/src/errors/index.ts
+++ packages/react-native-audio-api/src/errors/index.ts
@@ -1,2 +1,3 @@
 export { default as IndexSizeError } from './IndexSizeError';
+export { default as InvalidAccessError } from './InvalidAccessError';
 export { default as ValueRangeError } from './ValueRangeError';
