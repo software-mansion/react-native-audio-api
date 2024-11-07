d75ddeefb6d14eb4085d98dc96ce4a7518ac6cda
Maciej Makowski <maciej.makowski@swmansion.com>
2024-11-07 12:06:43 +0100

    feat: added props

--- packages/react-native-audio-api/common/cpp/core/PeriodicWave.h
+++ packages/react-native-audio-api/common/cpp/core/PeriodicWave.h
@@ -1,5 +1,18 @@
 #pragma once
 
-class PeriodicWave {
+namespace audioapi {
+    class PeriodicWave {
 
-};
+    private:
+        explicit PeriodicWave(unsigned sampleRate);
+
+        // determines the time resolution of the waveform.
+        unsigned sampleRate_;
+        // determines number of frequency segments (or bands) the signal is divided.
+        unsigned numberOfRanges_;
+        // the lowest frequency (in hertz) where playback will include all of the partials.
+        float lowestFundamentalFrequency;
+        // scaling factor used to adjust size of period of waveform to the sample rate.
+        float rateScale_;
+    };
+} // namespace audioapi
