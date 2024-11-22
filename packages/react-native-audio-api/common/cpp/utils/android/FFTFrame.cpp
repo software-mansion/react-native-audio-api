6395782c1213b824f29b8ef7036239ca569ec9ca
Maciej Makowski <maciej.makowski@swmansion.com>
2024-11-22 18:13:00 +0100

    fix: fixed buildi kfr

--- packages/react-native-audio-api/common/cpp/utils/android/FFTFrame.cpp
+++ packages/react-native-audio-api/common/cpp/utils/android/FFTFrame.cpp
@@ -1,6 +1,8 @@
 #if defined(ANDROID)
 #include "FFTFrame.h"
 #include <kfr/dft.hpp>
+#include <kfr/io.hpp>
+#include <kfr/dsp.hpp>
 
 namespace audioapi {
 FFTFrame::FFTFrame(int size) {
@@ -16,51 +18,24 @@ FFTFrame::~FFTFrame() {
 }
 
 void FFTFrame::forward(float *data) {
-//    auto* frame = (fftwf_complex*) fftwf_malloc(sizeof(fftwf_complex) * (size_ / 2 + 1));
-//
-//    // Create FFTW plan for real to complex forward transform
-//    fftwf_plan plan_forward = fftwf_plan_dft_r2c_1d(size_, data, frame, FFTW_ESTIMATE);
-//
-//    // Execute the forward FFT
-//    fftwf_execute(plan_forward);
-//
-//    // Extract real and imaginary parts from the FFTW output
-//    for (int i = 0; i < size_ / 2 + 1; i++) {
-//        realData_[i] = frame[i][0];
-//        imaginaryData_[i] = frame[i][1];
-//    }
-//
-//    // Cleanup FFTW resources
-//    fftwf_destroy_plan(plan_forward);
-//    fftwf_free(frame);
-//
-//    // Scale the FFT data by 0.5, just like with vDSP
-//    VectorMath::multiplyByScalar(realData_, 0.5f, realData_, size_ / 2);
-//    VectorMath::multiplyByScalar(imaginaryData_, 0.5f, imaginaryData_, size_ / 2);
+
 }
 
 void FFTFrame::inverse(float *data) {
-//    // Allocate memory for the FFTW complex frequency-domain data
-//    auto* frame = (fftwf_complex*) fftwf_malloc(sizeof(fftwf_complex) * (size_ / 2 + 1));
-//
-//    // Initialize input data for inverse transformation
-//    for (int i = 0; i < size_ / 2 + 1; i++) {
-//        frame[i][0] = realData_[i];
-//        frame[i][1] = imaginaryData_[i];
-//    }
-//
-//    // Create FFTW plan for complex to real inverse transform
-//    fftwf_plan plan_backward = fftwf_plan_dft_c2r_1d(size_, frame, data, FFTW_ESTIMATE);
-//
-//    // Execute the inverse FFT
-//    fftwf_execute(plan_backward);
-//
-//    // Normalize the result by dividing by the size
-//    VectorMath::multiplyByScalar(data, 1.0f / static_cast<float>(size_) , data, size_);
-//
-//    // Cleanup FFTW resources
-//    fftwf_destroy_plan(plan_backward);
-//    fftwf_free(frame);
+    kfr::dft_plan_real<float> plan(4096);
+    kfr::univector<kfr::complex<float>, 4096> in;
+    kfr::univector<float, 4096> out;
+
+    for(int i = 0; i < 4096; i++) {
+        in[i] = kfr::complex<float>(realData_[i], imaginaryData_[i]);
+    }
+
+    kfr::univector<kfr::u8> temp(plan.temp_size);
+    plan.execute(out, in, temp);
+
+    for(int i = 0; i < 4096; i++) {
+        data[i] = out[i]/ static_cast<float>(size_);
+    }
 }
 } // namespace audioapi
 #endif
