68545b631d157962af21e4551c8ec9eb1b93caf9
Michal Sek <michal.sek@swmansion.com>
2024-12-06 18:07:12 +0100

    feat: basic docusaurus setup

---
+++ packages/audiodocs/src/theme/Navbar/index.tsx
@@ -0,0 +1,18 @@
+// import React from 'react';
+// import { Navbar } from '@swmansion/t-rex-ui';
+// import useBaseUrl from '@docusaurus/useBaseUrl';
+
+// export default function NavbarWrapper(props) {
+//   const titleImages = {
+//     light: useBaseUrl('/img/title.svg'),
+//     dark: useBaseUrl('/img/title-dark.svg'),
+//   };
+
+//   const heroImages = {
+//     logo: useBaseUrl('/img/logo-hero.svg'),
+//     title: useBaseUrl('/img/title-hero.svg'),
+//   };
+//   return (
+//     <Navbar heroImages={heroImages} titleImages={titleImages} {...props} />
+//   );
+// }
