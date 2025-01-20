cbd172bf3a3892e4df14e5bb958d5a49fce0c5e4
Maciej Makowski <maciej.makowski2608@gmail.com>
2025-01-20 10:16:13 +0100

    feat: added ts error handling

--- packages/react-native-audio-api/src/core/AnalyserNode.ts
+++ packages/react-native-audio-api/src/core/AnalyserNode.ts
@@ -1,13 +1,13 @@
+import { IndexSizeError } from '../errors';
 import { IAnalyserNode } from '../interfaces';
 import AudioNode from './AudioNode';
 import BaseAudioContext from './BaseAudioContext';
 
 export default class AnalyserNode extends AudioNode {
-  fftSize: number;
   readonly frequencyBinCount: number;
-  minDecibels: number;
-  maxDecibels: number;
-  smoothingTimeConstant: number;
+  private static allowedFFTSize: number[] = [
+    32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768,
+  ];
 
   constructor(context: BaseAudioContext, analyser: IAnalyserNode) {
     super(context, analyser);
@@ -19,19 +19,75 @@ export default class AnalyserNode extends AudioNode {
     this.smoothingTimeConstant = analyser.smoothingTimeConstant;
   }
 
-  getFloatFrequencyData(array: number[]): void {
+  public get fftSize(): number {
+    return (this.node as IAnalyserNode).fftSize;
+  }
+
+  public set fftSize(value: number) {
+    if (!AnalyserNode.allowedFFTSize.includes(value)) {
+      throw new IndexSizeError(
+        `Provided value (${value}) must be a power of 2 2^5 and 2^15`
+      );
+    }
+
+    (this.node as IAnalyserNode).fftSize = value;
+  }
+
+  public get minDecibels(): number {
+    return (this.node as IAnalyserNode).minDecibels;
+  }
+
+  public set minDecibels(value: number) {
+    if (value >= this.maxDecibels) {
+      throw new IndexSizeError(
+        `The minDecibels value (${value}) must be less than maxDecibels`
+      );
+    }
+
+    (this.node as IAnalyserNode).minDecibels = value;
+  }
+
+  public get maxDecibels(): number {
+    return (this.node as IAnalyserNode).maxDecibels;
+  }
+
+  public set maxDecibels(value: number) {
+    if (value <= this.minDecibels) {
+      throw new IndexSizeError(
+        `The maxDecibels value (${value}) must be greater than minDecibels`
+      );
+    }
+
+    (this.node as IAnalyserNode).maxDecibels = value;
+  }
+
+  public get smoothingTimeConstant(): number {
+    return (this.node as IAnalyserNode).smoothingTimeConstant;
+  }
+
+  public set smoothingTimeConstant(value: number) {
+    if (value < 0 || value > 1) {
+      throw new IndexSizeError(
+        `The smoothingTimeConstant value (${value}) must be between 0 and 1`
+      );
+    }
+
+    (this.node as IAnalyserNode).smoothingTimeConstant = value;
+  }
+
+  public getFloatFrequencyData(array: number[]): void {
     (this.node as IAnalyserNode).getFloatFrequencyData(array);
   }
 
-  getByteFrequencyData(array: number[]): void {
+  public getByteFrequencyData(array: number[]): void {
     (this.node as IAnalyserNode).getByteFrequencyData(array);
   }
 
-  getFloatTimeDomainData(array: number[]): void {
+  public getFloatTimeDomainData(array: number[]): void {
     (this.node as IAnalyserNode).getFloatTimeDomainData(array);
   }
 
-  getByteTimeDomainData(array: number[]): void {
+  public getByteTimeDomainData(array: number[]): void {
     (this.node as IAnalyserNode).getByteTimeDomainData(array);
   }
 }
