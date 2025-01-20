2b1ac30de1ce29dedaf9d93c24362e2df862603e
Maciej Makowski <maciej.makowski2608@gmail.com>
2025-01-20 10:16:39 +0100

    feat: added utils for float to uint8 conversion

--- packages/react-native-audio-api/common/cpp/utils/AudioUtils.cpp
+++ packages/react-native-audio-api/common/cpp/utils/AudioUtils.cpp
@@ -23,4 +23,20 @@ float linearInterpolate(
       factor * (source[secondIndex] - source[firstIndex]);
 }
 
+float convertBytetoFloat(const uint8_t data) {
+    return static_cast<float>(data) / 128.0f - 1.0f;
+}
+
+uint8_t convertFloatToByte(const float data) {
+    float scaledValue = 128 * (data + 1);
+
+    if (scaledValue < 0) {
+        scaledValue = 0;
+    }
+    if (scaledValue > UINT8_MAX) {
+        scaledValue = UINT8_MAX;
+    }
+
+    return static_cast<uint8_t>(scaledValue);
+}
 } // namespace audioapi::AudioUtils
--- packages/react-native-audio-api/common/cpp/utils/AudioUtils.h
+++ packages/react-native-audio-api/common/cpp/utils/AudioUtils.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <cstddef>
+#include <cstdint>
 
 namespace audioapi::AudioUtils {
 size_t timeToSampleFrame(double time, int sampleRate);
@@ -8,5 +9,9 @@ size_t timeToSampleFrame(double time, int sampleRate);
 double sampleFrameToTime(int sampleFrame, int sampleRate);
 
 float linearInterpolate(const float *source, size_t firstIndex, size_t secondIndex, float factor);
+
+float convertBytetoFloat(const uint8_t data);
+
+uint8_t convertFloatToByte(const float data);
 
 } // namespace audioapi::AudioUtils
