c92bc8ea23a14f725a9b6450bc11ecfcfb366038
Maciej Makowski <maciej.makowski2608@gmail.com>
2025-01-20 15:14:50 +0100

    feat: added very first version of AudioVisualizer example

--- apps/common-app/src/examples/AudioVisualizer/AudioVisualizer.tsx
+++ apps/common-app/src/examples/AudioVisualizer/AudioVisualizer.tsx
@@ -1,33 +1,45 @@
 import React, { useState, useEffect, useRef } from 'react';
+import * as FileSystem from 'expo-file-system';
 import {
   AudioContext,
   AnalyserNode,
-  OscillatorNode,
+  AudioBuffer,
+  AudioBufferSourceNode,
 } from 'react-native-audio-api';
+import { ActivityIndicator } from 'react-native';
 
 import Layout from './Layout';
 import FreqTimeChart from './FreqTimeChart';
 import { Container, Button } from '../../components';
 
+const URL =
+  'https://maciejmakowski2003.github.io/audio-samples/sounds/chrono.mp3';
+
 const AudioVisualizer: React.FC = () => {
   const [isPlaying, setIsPlaying] = useState(false);
-  const [data, setData] = useState<number[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const [times, setTimes] = useState<number[]>([]);
+  const [freqs, setFreqs] = useState<number[]>([]);
 
   const audioContextRef = useRef<AudioContext | null>(null);
   const analyserRef = useRef<AnalyserNode | null>(null);
-  const oscillatorRef = useRef<OscillatorNode | null>(null);
+  const bufferSourceRef = useRef<AudioBufferSourceNode | null>(null);
+  const audioBufferRef = useRef<AudioBuffer | null>(null);
 
   const handlePlayPause = () => {
     if (isPlaying) {
-      oscillatorRef.current?.stop(0);
+      bufferSourceRef.current?.stop();
     } else {
       if (!audioContextRef.current || !analyserRef.current) {
         return;
       }
 
-      oscillatorRef.current = audioContextRef.current?.createOscillator();
-      oscillatorRef.current.connect(analyserRef.current);
-      oscillatorRef.current?.start(0);
+      bufferSourceRef.current = audioContextRef.current.createBufferSource();
+      bufferSourceRef.current.buffer = audioBufferRef.current;
+      bufferSourceRef.current.connect(analyserRef.current);
+
+      bufferSourceRef.current.start();
 
       requestAnimationFrame(draw);
     }
@@ -41,9 +53,14 @@ const AudioVisualizer: React.FC = () => {
     }
 
     const bufferLength = analyserRef.current.frequencyBinCount;
-    const dataArray = new Array(bufferLength);
-    analyserRef.current.getByteTimeDomainData(dataArray);
-    setData(dataArray);
+
+    const timesArray = new Array(bufferLength);
+    analyserRef.current.getByteTimeDomainData(timesArray);
+    setTimes(timesArray);
+
+    const freqsArray = new Array(bufferLength);
+    analyserRef.current.getByteFrequencyData(freqsArray);
+    setFreqs(freqsArray);
 
     requestAnimationFrame(draw);
   };
@@ -63,6 +80,23 @@ const AudioVisualizer: React.FC = () => {
       analyserRef.current.connect(audioContextRef.current.destination);
     }
 
+    const fetchBuffer = async () => {
+      setIsLoading(true);
+      audioBufferRef.current = await FileSystem.downloadAsync(
+        URL,
+        FileSystem.documentDirectory + 'audio.mp3'
+      )
+        .then(({ uri }) => {
+          return uri.replace('file://', '');
+        })
+        .then((uri) => {
+          return audioContextRef.current!.decodeAudioDataSource(uri);
+        });
+      setIsLoading(false);
+    };
+
+    fetchBuffer();
+
     return () => {
       audioContextRef.current?.close();
     };
@@ -70,10 +104,23 @@ const AudioVisualizer: React.FC = () => {
 
   return (
     <Container>
-      <Layout.Box>
-        <FreqTimeChart data={data} />
-      </Layout.Box>
-      <Button onPress={handlePlayPause} title={isPlaying ? 'Pause' : 'Play'} />
+      <Layout.Row>
+        <Layout.Box>
+          <FreqTimeChart data={times} />
+        </Layout.Box>
+      </Layout.Row>
+      <Layout.LineHorizontal />
+      <Layout.Row>
+        <Layout.Box>
+          <FreqTimeChart data={freqs} />
+        </Layout.Box>
+      </Layout.Row>
+      {isLoading && <ActivityIndicator color="#FFFFFF" />}
+      <Button
+        onPress={handlePlayPause}
+        title={isPlaying ? 'Pause' : 'Play'}
+        disabled={!audioBufferRef.current}
+      />
     </Container>
   );
 };
