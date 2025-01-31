215d2538bfb7620a5127d26fe4a88181b65f6c4e
Maciej Makowski <maciej.makowski2608@gmail.com>
2025-01-31 09:02:59 +0100

    refactor: refactored types and constants in AudioBufferSourceNode

--- packages/react-native-audio-api/common/cpp/core/Constants.h
+++ packages/react-native-audio-api/common/cpp/core/Constants.h
@@ -6,15 +6,16 @@
 // https://webaudio.github.io/web-audio-api/
 
 namespace audioapi {
+// context
 constexpr int DEFAULT_SAMPLE_RATE = 48000;
 constexpr int RENDER_QUANTUM_SIZE = 128;
 constexpr int CHANNEL_COUNT = 2;
 
+// general
 constexpr float MOST_POSITIVE_SINGLE_FLOAT = static_cast<float>(std::numeric_limits<float>::max());
 constexpr float MOST_NEGATIVE_SINGLE_FLOAT = static_cast<float>(std::numeric_limits<float>::lowest());
 
 constexpr float NYQUIST_FREQUENCY = DEFAULT_SAMPLE_RATE / 2.0;
-static float MAX_DETUNE = 1200 * std::log2(MOST_POSITIVE_SINGLE_FLOAT);
 constexpr float MAX_GAIN = MOST_POSITIVE_SINGLE_FLOAT;
 constexpr float MAX_PAN = 1.0;
 constexpr float MAX_FILTER_Q = MOST_POSITIVE_SINGLE_FLOAT;
@@ -22,6 +23,10 @@ constexpr float MAX_FILTER_FREQUENCY = NYQUIST_FREQUENCY;
 constexpr float MIN_FILTER_FREQUENCY = 0.0;
 static float MAX_FILTER_GAIN = 40 * std::log10(MOST_POSITIVE_SINGLE_FLOAT);
 constexpr float MIN_FILTER_GAIN = -MAX_GAIN;
+
+//detune
+static float MAX_DETUNE = 1200 * std::log2(MOST_POSITIVE_SINGLE_FLOAT);
+static float MIN_DETUNE = -MAX_DETUNE;
 
 // analyser node
 constexpr size_t MAX_FFT_SIZE = 32768;
