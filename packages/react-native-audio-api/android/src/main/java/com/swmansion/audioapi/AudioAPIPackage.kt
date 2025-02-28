a1a1aeb4e9038318114e9be2d3a5c8392a485dc5
Maciej Makowski <maciej.makowski2608@gmail.com>
2025-02-28 11:50:48 +0100

    ci: trying to fix linting one more time xdd

--- packages/react-native-audio-api/android/src/main/java/com/swmansion/audioapi/AudioAPIPackage.kt
+++ packages/react-native-audio-api/android/src/main/java/com/swmansion/audioapi/AudioAPIPackage.kt
@@ -26,6 +26,7 @@ class AudioAPIPackage :
   override fun getReactModuleInfoProvider(): ReactModuleInfoProvider =
 
     ReactModuleInfoProvider(
+
       fun (): Map<String, ReactModuleInfo> =
         mapOf(
           AudioAPIModule.NAME to
