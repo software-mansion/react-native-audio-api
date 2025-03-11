c0def66d86299a0c8355011f07da2731cb511a4c
immigration9 <projgotham18@gmail.com>
2025-03-11 18:07:47 +0900

    Add rn level typescript file for OfflineAudioContext

---
+++ packages/react-native-audio-api/src/core/OfflineAudioContext.ts
@@ -0,0 +1,28 @@
+import { IAudioBuffer, IOfflineAudioContext } from '../interfaces';
+import BaseAudioContext from './BaseAudioContext';
+import { OfflineAudioContextOptions } from '../types';
+import { NotSupportedError } from '../errors';
+
+export default class OfflineAudioContext extends BaseAudioContext {
+  constructor(options?: OfflineAudioContextOptions) {
+    if (options && (options.sampleRate < 8000 || options.sampleRate > 96000)) {
+      throw new NotSupportedError(
+        `The provided sampleRate is not supported: ${options.sampleRate}`
+      );
+    }
+
+    super(global.createAudioContext(options?.sampleRate));
+  }
+
+  async suspend(): Promise<undefined> {
+    await (this.context as IOfflineAudioContext).suspend();
+  }
+
+  async resume(): Promise<undefined> {
+    await (this.context as IOfflineAudioContext).resume();
+  }
+
+  async startRendering(): Promise<IAudioBuffer> {
+    return await (this.context as IOfflineAudioContext).startRendering();
+  }
+}
