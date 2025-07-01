f43e66204394fb9b7d543987b2b316a92fb6e989
michal <dydmichal@gmail.com>
2025-07-01 16:08:05 +0200

    test: added ci

--- packages/react-native-audio-api/common/cpp/test/RunTests.sh
+++ packages/react-native-audio-api/common/cpp/test/RunTests.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
-# Step 1: Configure CMake project
+# Step 1: Move to the script's directory
+cd "$(dirname "$0")"
+
+# Step 2: Configure CMake project
 cmake -S . -B build
 
-# Step 2: Build the project
+# Step 3: Build the project
 cmake --build build
 
-# Step 3: Run the test binary
+# Step 4: Run the test binary
 cd build
 ./tests
 cd ..
 
-# Step 4: Clean up build directory
+# Step 5: Clean up build directory
 rm -rf build/
