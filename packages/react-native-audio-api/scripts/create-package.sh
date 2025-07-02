cf2e7c255e612ea72f1b5deae95cd420ef6cff8f
michal <dydmichal@gmail.com>
2025-07-02 16:24:16 +0200

    chore: script for nigthly build

--- packages/react-native-audio-api/scripts/create-package.sh
+++ packages/react-native-audio-api/scripts/create-package.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 
 yarn install --immutable
+
+if [ $# -ge 1 ] && [ "$1" = "generate_nightly_version" ]; then
+    VERSION=$(jq -r '.version' package.json)
+    IFS='.' read -r MAJOR MINOR PATCH <<< "$VERSION" 
+    GIT_COMMIT=$(git rev-parse HEAD)
+    DATE=$(date +%Y%m%d)
+    NIGHTLY_UNIQUE_NAME="${GIT_COMMIT:0:7}-$DATE"
+    sed -i '' "3s/.*/  \"version\": \"$MAJOR.$MINOR.$PATCH-nightly-$NIGHTLY_UNIQUE_NAME\",/" package.json
+fi
+
 yarn bob build
 
 npm pack
+
+if [ $# -ge 1 ] && [ "$1" = "generate_nightly_version" ]; then
+    sed -i '' "3s/.*/  \"version\": \"$MAJOR.$MINOR.$PATCH\",/" package.json
+fi
 
 echo "Done!"
