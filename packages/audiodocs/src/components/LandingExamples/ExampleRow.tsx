e32e3a57d4c93bc19eae9f82b6f7f9e2f4400dde
Michał Sęk <michal.sek@swmansion.com>
2025-07-16 09:37:10 +0200

    feat: examples struct

--- packages/audiodocs/src/components/LandingExamples/ExampleRow.tsx
+++ packages/audiodocs/src/components/LandingExamples/ExampleRow.tsx
@@ -1,3 +1,7 @@
+import React from "react";
+
+import styles from "./styles.module.css";
+import clsx from "clsx";
 
 interface ExampleRowProps {
   title: string;
@@ -6,9 +10,17 @@ interface ExampleRowProps {
   inverted?: boolean;
 }
 
-const ExampleRow: React.FC<ExampleRowProps> = ({ title, description, Component }) => {
+const ExampleRow: React.FC<ExampleRowProps> = ({ title, description, Component, inverted }) => {
   return (
-    <div></div>
+    <div className={clsx(styles.exampleRow, { [styles.inverted]: inverted })}>
+      <div className={styles.exampleDetails}>
+        <h4 className={styles.exampleDetailsTitle}>{title}</h4>
+        <p className={styles.exampleDetailsDescription}>{description}</p>
+      </div>
+      <div className={styles.exampleContainer}>
+        {!!Component && <Component />}
+      </div>
+    </div>
   );
 };
 
