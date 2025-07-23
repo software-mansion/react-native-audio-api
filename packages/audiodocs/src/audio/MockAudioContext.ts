435c1b5262068ead09c0297dbbb75d98a628c7b2
Michał Sęk <michal.sek@swmansion.com>
2025-07-23 09:07:56 +0200

    fix: ssr

--- packages/audiodocs/src/audio/MockAudioContext.ts
+++ packages/audiodocs/src/audio/MockAudioContext.ts
@@ -8,6 +8,13 @@ const MockAudioContext = {
     gain: { value: 1 },
     connect: () => {},
   }),
+  createBiquadFilter: () => ({
+    type: "lowpass",
+    frequency: { value: 1000, setValueAtTime: () => {} },
+    Q: { value: 1, setValueAtTime: () => {} },
+    gain: { value: 0, setValueAtTime: () => {} },
+    connect: () => {},
+  }),
   createBufferSource: () => ({
     buffer: null,
     connect: () => {},
