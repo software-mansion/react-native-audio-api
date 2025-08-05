e3646330e3971a5a7a283f56e1049fb58605cdea
Alicja Wojciechowska <alicjawojciechowska555@gmail.com>
2025-08-05 16:17:29 +0200

    feat: remove source code of external libraries

--- packages/react-native-audio-api/libs/vorbis/build-ios-simulator/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/alicja/react-native-audio-api/packages/react-native-audio-api/libs/vorbis
-# Build directory: /Users/alicja/react-native-audio-api/packages/react-native-audio-api/libs/vorbis/build-ios-simulator
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("lib")
-subdirs("test")
--- packages/react-native-audio-api/libs/vorbis/build-ios-simulator/lib/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/alicja/react-native-audio-api/packages/react-native-audio-api/libs/vorbis/lib
-# Build directory: /Users/alicja/react-native-audio-api/packages/react-native-audio-api/libs/vorbis/build-ios-simulator/lib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
