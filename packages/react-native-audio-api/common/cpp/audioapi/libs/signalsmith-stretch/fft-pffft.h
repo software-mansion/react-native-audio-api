79d7be8a479cf40d11102b0df6ea2c0205bd25e6
maciejmakowski2003 <maciej.makowski2608@gmail.com>
2025-08-21 14:48:58 +0200

    fix: fixed includes

--- packages/react-native-audio-api/common/cpp/audioapi/libs/signalsmith-stretch/fft-pffft.h
+++ packages/react-native-audio-api/common/cpp/audioapi/libs/signalsmith-stretch/fft-pffft.h
@@ -11,6 +11,7 @@
 #include <cmath>
 #include <complex>
 #include <cassert>
+#include <cstring>
 
 namespace signalsmith { namespace linear {
 
--- packages/react-native-audio-api/common/cpp/audioapi/libs/signalsmith-stretch/fft.h
+++ packages/react-native-audio-api/common/cpp/audioapi/libs/signalsmith-stretch/fft.h
@@ -4,6 +4,7 @@
 #include <complex>
 #include <vector>
 #include <cmath>
+#include <cstring>
 
 #if defined(__FAST_MATH__) && (__apple_build_version__ >= 16000000) && (__apple_build_version__ <= 16000099) && !defined(SIGNALSMITH_IGNORE_BROKEN_APPLECLANG)
 #	error Apple Clang 16.0.0 generates incorrect SIMD for ARM. If you HAVE to use this version of Clang, turn off -ffast-math.
