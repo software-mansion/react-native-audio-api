0796dd67d429e6b2d51d564abbc118dc0cfb4795
Alicja Wojciechowska <alicjawojciechowska555@gmail.com>
2025-09-18 12:18:40 +0200

    refactor: revert previous formatting

--- packages/react-native-audio-api/common/cpp/audioapi/libs/ffmpeg/FFmpegDecoding.h
+++ packages/react-native-audio-api/common/cpp/audioapi/libs/ffmpeg/FFmpegDecoding.h
@@ -14,19 +14,19 @@
 #include <vector>
 
 extern "C" {
-#include <libavcodec/avcodec.h>
-#include <libavformat/avformat.h>
-#include <libavutil/opt.h>
-#include <libswresample/swresample.h>
+    #include <libavcodec/avcodec.h>
+    #include <libavformat/avformat.h>
+    #include <libavutil/opt.h>
+    #include <libswresample/swresample.h>
 }
 
 namespace audioapi::ffmpegdecoding {
 // Custom IO context for reading from memory
 struct MemoryIOContext {
-  const uint8_t *data;
-  size_t size;
-  size_t pos;
-};
+    const uint8_t *data;
+    size_t size;
+    size_t pos;
+  };
 
 int read_packet(void *opaque, uint8_t *buf, int buf_size);
 int64_t seek_packet(void *opaque, int64_t offset, int whence);
@@ -34,4 +34,4 @@ std::vector<int16_t> readAllPcmFrames(AVFormatContext *fmt_ctx, AVCodecContext *
 std::vector<int16_t> decodeWithMemoryBlock(const void *data, size_t size, const int channel_count, int sample_rate);
 std::vector<int16_t> decodeWithFilePath(const std::string &path, const int channel_count, int sample_rate);
 
-} // namespace audioapi::ffmpegdecoding
+} // namespace audioapi::ffmpegdecoder
