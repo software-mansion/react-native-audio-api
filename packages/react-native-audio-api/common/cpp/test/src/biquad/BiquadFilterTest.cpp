0bbdc1f2c8beb6a797c49ab2e8b348a2ef145441
Alicja Wojciechowska <alicjawojciechowska555@gmail.com>
2025-11-10 16:45:17 +0000

    test: update biquad tests v2

--- packages/react-native-audio-api/common/cpp/test/src/biquad/BiquadFilterTest.cpp
+++ packages/react-native-audio-api/common/cpp/test/src/biquad/BiquadFilterTest.cpp
@@ -15,7 +15,7 @@ void BiquadFilterTest::expectCoefficientsNear(
 
 void BiquadFilterTest::testLowpass(float frequency, float Q) {
   auto node = std::make_shared<BiquadFilterNode>(
-      "lowpass", 1.0f, 0.0f, 350.0f, 0.0f, context.get());
+      context.get(), "lowpass", 1.0f, 0.0f, 350.0f, 0.0f);
   float normalizedFrequency = frequency / nyquistFrequency;
 
   node->setLowpassCoefficients(normalizedFrequency, Q);
@@ -24,8 +24,8 @@ void BiquadFilterTest::testLowpass(float frequency, float Q) {
 }
 
 void BiquadFilterTest::testHighpass(float frequency, float Q) {
-    auto node = std::make_shared<BiquadFilterNode>(
-      "lowpass", 1.0f, 0.0f, 350.0f, 0.0f, context.get());
+  auto node = std::make_shared<BiquadFilterNode>(
+      context.get(), "lowpass", 1.0f, 0.0f, 350.0f, 0.0f);
   float normalizedFrequency = frequency / nyquistFrequency;
 
   node->setHighpassCoefficients(normalizedFrequency, Q);
@@ -34,8 +34,8 @@ void BiquadFilterTest::testHighpass(float frequency, float Q) {
 }
 
 void BiquadFilterTest::testBandpass(float frequency, float Q) {
-    auto node = std::make_shared<BiquadFilterNode>(
-      "lowpass", 1.0f, 0.0f, 350.0f, 0.0f, context.get());
+  auto node = std::make_shared<BiquadFilterNode>(
+      context.get(), "lowpass", 1.0f, 0.0f, 350.0f, 0.0f);
   float normalizedFrequency = frequency / nyquistFrequency;
 
   node->setBandpassCoefficients(normalizedFrequency, Q);
@@ -44,8 +44,8 @@ void BiquadFilterTest::testBandpass(float frequency, float Q) {
 }
 
 void BiquadFilterTest::testNotch(float frequency, float Q) {
-    auto node = std::make_shared<BiquadFilterNode>(
-      "lowpass", 1.0f, 0.0f, 350.0f, 0.0f, context.get());
+  auto node = std::make_shared<BiquadFilterNode>(
+      context.get(), "lowpass", 1.0f, 0.0f, 350.0f, 0.0f);
   float normalizedFrequency = frequency / nyquistFrequency;
 
   node->setNotchCoefficients(normalizedFrequency, Q);
@@ -54,8 +54,8 @@ void BiquadFilterTest::testNotch(float frequency, float Q) {
 }
 
 void BiquadFilterTest::testAllpass(float frequency, float Q) {
-    auto node = std::make_shared<BiquadFilterNode>(
-      "lowpass", 1.0f, 0.0f, 350.0f, 0.0f, context.get());
+  auto node = std::make_shared<BiquadFilterNode>(
+      context.get(), "lowpass", 1.0f, 0.0f, 350.0f, 0.0f);
   float normalizedFrequency = frequency / nyquistFrequency;
 
   node->setAllpassCoefficients(normalizedFrequency, Q);
@@ -64,8 +64,8 @@ void BiquadFilterTest::testAllpass(float frequency, float Q) {
 }
 
 void BiquadFilterTest::testPeaking(float frequency, float Q, float gain) {
-    auto node = std::make_shared<BiquadFilterNode>(
-      "lowpass", 1.0f, 0.0f, 350.0f, 0.0f, context.get());
+  auto node = std::make_shared<BiquadFilterNode>(
+      context.get(), "lowpass", 1.0f, 0.0f, 350.0f, 0.0f);
   float normalizedFrequency = frequency / nyquistFrequency;
 
   node->setPeakingCoefficients(normalizedFrequency, Q, gain);
@@ -74,8 +74,8 @@ void BiquadFilterTest::testPeaking(float frequency, float Q, float gain) {
 }
 
 void BiquadFilterTest::testLowshelf(float frequency, float gain) {
-    auto node = std::make_shared<BiquadFilterNode>(
-      "lowpass", 1.0f, 0.0f, 350.0f, 0.0f, context.get());
+  auto node = std::make_shared<BiquadFilterNode>(
+      context.get(), "lowpass", 1.0f, 0.0f, 350.0f, 0.0f);
   float normalizedFrequency = frequency / nyquistFrequency;
 
   node->setLowshelfCoefficients(normalizedFrequency, gain);
@@ -84,8 +84,8 @@ void BiquadFilterTest::testLowshelf(float frequency, float gain) {
 }
 
 void BiquadFilterTest::testHighshelf(float frequency, float gain) {
-    auto node = std::make_shared<BiquadFilterNode>(
-      "lowpass", 1.0f, 0.0f, 350.0f, 0.0f, context.get());
+  auto node = std::make_shared<BiquadFilterNode>(
+      context.get(), "lowpass", 1.0f, 0.0f, 350.0f, 0.0f);
   float normalizedFrequency = frequency / nyquistFrequency;
 
   node->setHighshelfCoefficients(normalizedFrequency, gain);
@@ -232,8 +232,8 @@ TEST_P(BiquadFilterGainTest, SetHighshelfCoefficients) {
 }
 
 TEST_F(BiquadFilterTest, GetFrequencyResponse) {
-    auto node = std::make_shared<BiquadFilterNode>(
-      "lowpass", 1.0f, 0.0f, 350.0f, 0.0f, context.get());
+  auto node = std::make_shared<BiquadFilterNode>(
+      context.get(), "lowpass", 1.0f, 0.0f, 350.0f, 0.0f);
 
   float frequency = 1000.0f;
   float Q = 1.0f;
