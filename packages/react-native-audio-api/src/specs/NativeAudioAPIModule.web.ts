f058299d6cf3a141e769146b487a867c135fab91
maciejmakowski2003 <maciej.makowski2608@gmail.com>
2025-12-23 16:18:05 +0100

    fix: fixed audio api module codegen generation

--- packages/react-native-audio-api/src/specs/NativeAudioAPIModule.web.ts
+++ packages/react-native-audio-api/src/specs/NativeAudioAPIModule.web.ts
@@ -1,5 +1,58 @@
-import type { Spec } from './ModuleInterfaces';
-import { PermissionStatus } from '../system/types';
+import { TurboModule } from 'react-native';
+import { AudioDevicesInfo, PermissionStatus } from '../system/types';
+
+// copy of spec from NativeAudioAPIModule.ts
+type OptionsMap = {
+  [key: string]: string | boolean | number | undefined;
+};
+type NotificationOpResponse = { success: boolean; error?: string };
+type NotificationType = 'playback' | 'recording' | 'simple';
+
+interface Spec extends TurboModule {
+  install(): boolean;
+  getDevicePreferredSampleRate(): number;
+
+  // AVAudioSession management
+  setAudioSessionActivity(enabled: boolean): Promise<boolean>;
+  setAudioSessionOptions(
+    category: string,
+    mode: string,
+    options: Array<string>,
+    allowHaptics: boolean
+  ): void;
+  disableSessionManagement(): void;
+
+  // Remote commands, system events and interruptions
+  observeAudioInterruptions(enabled: boolean): void;
+  activelyReclaimSession(enabled: boolean): void;
+  observeVolumeChanges(enabled: boolean): void;
+
+  // Permissions
+  requestRecordingPermissions(): Promise<PermissionStatus>;
+  checkRecordingPermissions(): Promise<PermissionStatus>;
+  requestNotificationPermissions(): Promise<PermissionStatus>;
+  checkNotificationPermissions(): Promise<PermissionStatus>;
+
+  // Audio devices
+  getDevicesInfo(): Promise<AudioDevicesInfo>;
+
+  // New notification system
+  registerNotification(
+    type: NotificationType,
+    key: string
+  ): Promise<NotificationOpResponse>;
+  showNotification(
+    key: string,
+    options: OptionsMap
+  ): Promise<NotificationOpResponse>;
+  updateNotification(
+    key: string,
+    options: OptionsMap
+  ): Promise<NotificationOpResponse>;
+  hideNotification(key: string): Promise<NotificationOpResponse>;
+  unregisterNotification(key: string): Promise<NotificationOpResponse>;
+  isNotificationActive(key: string): Promise<boolean>;
+}
 
 const mockAsync =
   <T>(value: T) =>
