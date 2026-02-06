60ea709e81193ebfe2f770c718f3ff1b414b6ac7
maciejmakowski2003 <maciej.makowski2608@gmail.com>
2026-02-06 18:04:47 +0100

    ci: lint

--- packages/react-native-audio-api/common/cpp/audioapi/utils/AudioArrayBuffer.hpp
+++ packages/react-native-audio-api/common/cpp/audioapi/utils/AudioArrayBuffer.hpp
@@ -9,7 +9,7 @@
 using JsiBuffer = facebook::jsi::MutableBuffer;
 #else
 // Dummy class to inherit from nothing if testing
-  struct JsiBuffer {};
+struct JsiBuffer {};
 #endif
 
 namespace audioapi {
@@ -20,11 +20,19 @@ class AudioArrayBuffer : public JsiBuffer, public AudioArray {
   AudioArrayBuffer(const float *data, size_t size) : AudioArray(data, size) {};
 
 #if !RN_AUDIO_API_TEST
-  [[nodiscard]] size_t size() const override { return size_ * sizeof(float); }
-  uint8_t *data() override { return reinterpret_cast<uint8_t *>(data_.get()); }
+  [[nodiscard]] size_t size() const override {
+    return size_ * sizeof(float);
+  }
+  uint8_t *data() override {
+    return reinterpret_cast<uint8_t *>(data_.get());
+  }
 #else
-    [[nodiscard]] size_t size() const { return size_ * sizeof(float); }
-    uint8_t *data() { return reinterpret_cast<uint8_t *>(data_.get()); }
+  [[nodiscard]] size_t size() const {
+    return size_ * sizeof(float);
+  }
+  uint8_t *data() {
+    return reinterpret_cast<uint8_t *>(data_.get());
+  }
 #endif
 };
 
