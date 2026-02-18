5ff280030153a62e3bd466378274ad0b769c53a3
Michał Sęk <michal.sek@swmansion.com>
2026-02-18 17:07:47 +0100

    feat: use normal version for prebuilt binaries

--- packages/react-native-audio-api/scripts/download-prebuilt-binaries.sh
+++ packages/react-native-audio-api/scripts/download-prebuilt-binaries.sh
@@ -2,7 +2,7 @@
 # Script to download and unzip prebuilt native binaries for React Native.
 
 MAIN_DOWNLOAD_URL="https://github.com/software-mansion-labs/rn-audio-libs/releases/download"
-TAG="v3.0.0-alpha.0"
+TAG="v3.0.0"
 DOWNLOAD_NAMES=(
     "android.zip"
     "ffmpeg_ios.zip"
