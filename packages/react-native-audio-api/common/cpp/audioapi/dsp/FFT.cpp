bd3c1325c016f02452e6a21b8ecfc77cb09650d8
michal <dydmichal@gmail.com>
2026-03-16 17:51:36 +0100

    fix: restore fft destructor

--- packages/react-native-audio-api/common/cpp/audioapi/dsp/FFT.cpp
+++ packages/react-native-audio-api/common/cpp/audioapi/dsp/FFT.cpp
@@ -7,4 +7,9 @@ FFT::FFT(int size)
       pffftSetup_(pffft_new_setup(size_, PFFFT_REAL)),
       work_(reinterpret_cast<float *>(pffft_aligned_malloc(size_ * sizeof(float)))) {}
 
+FFT::~FFT() {
+  pffft_destroy_setup(pffftSetup_);
+  pffft_aligned_free(work_);
+}
+
 } // namespace audioapi::dsp
