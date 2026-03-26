f151efa610abf90fa29dcecb0c187d585641bc9b
maciejmakowski2003 <maciejmakowski2003@users.noreply.github.com>
2026-03-26 10:27:08 +0100

    ci: yarn format

--- packages/react-native-audio-api/common/cpp/audioapi/HostObjects/destinations/AudioDestinationNodeHostObject.cpp
+++ packages/react-native-audio-api/common/cpp/audioapi/HostObjects/destinations/AudioDestinationNodeHostObject.cpp
@@ -10,22 +10,23 @@ AudioDestinationNodeHostObject::AudioDestinationNodeHostObject(
     utils::graph::HostGraph::Node *node,
     std::shared_ptr<AudioDestinationNode> destination,
     const AudioDestinationOptions &options)
-    : node_(node), destination_(std::move(destination)),
+    : node_(node),
+      destination_(std::move(destination)),
       numberOfInputs_(options.numberOfInputs),
       numberOfOutputs_(options.numberOfOutputs),
       channelCount_(options.channelCount),
       channelCountMode_(options.channelCountMode),
       channelInterpretation_(options.channelInterpretation) {
-    addGetters(
-        JSI_EXPORT_PROPERTY_GETTER(AudioDestinationNodeHostObject, numberOfInputs),
-        JSI_EXPORT_PROPERTY_GETTER(AudioDestinationNodeHostObject, numberOfOutputs),
-        JSI_EXPORT_PROPERTY_GETTER(AudioDestinationNodeHostObject, channelCount),
-        JSI_EXPORT_PROPERTY_GETTER(AudioDestinationNodeHostObject, channelCountMode),
-        JSI_EXPORT_PROPERTY_GETTER(AudioDestinationNodeHostObject, channelInterpretation));
+  addGetters(
+      JSI_EXPORT_PROPERTY_GETTER(AudioDestinationNodeHostObject, numberOfInputs),
+      JSI_EXPORT_PROPERTY_GETTER(AudioDestinationNodeHostObject, numberOfOutputs),
+      JSI_EXPORT_PROPERTY_GETTER(AudioDestinationNodeHostObject, channelCount),
+      JSI_EXPORT_PROPERTY_GETTER(AudioDestinationNodeHostObject, channelCountMode),
+      JSI_EXPORT_PROPERTY_GETTER(AudioDestinationNodeHostObject, channelInterpretation));
 
-    addFunctions(
-        JSI_EXPORT_FUNCTION(AudioDestinationNodeHostObject, connect),
-        JSI_EXPORT_FUNCTION(AudioDestinationNodeHostObject, disconnect));
+  addFunctions(
+      JSI_EXPORT_FUNCTION(AudioDestinationNodeHostObject, connect),
+      JSI_EXPORT_FUNCTION(AudioDestinationNodeHostObject, disconnect));
 }
 
 JSI_PROPERTY_GETTER_IMPL(AudioDestinationNodeHostObject, numberOfInputs) {
@@ -37,17 +38,17 @@ JSI_PROPERTY_GETTER_IMPL(AudioDestinationNodeHostObject, numberOfOutputs) {
 }
 
 JSI_PROPERTY_GETTER_IMPL(AudioDestinationNodeHostObject, channelCount) {
-    return {static_cast<int>(channelCount_)};
+  return {static_cast<int>(channelCount_)};
 }
 
 JSI_PROPERTY_GETTER_IMPL(AudioDestinationNodeHostObject, channelCountMode) {
-    return jsi::String::createFromUtf8(
-            runtime, js_enum_parser::channelCountModeToString(channelCountMode_));
+  return jsi::String::createFromUtf8(
+      runtime, js_enum_parser::channelCountModeToString(channelCountMode_));
 }
 
 JSI_PROPERTY_GETTER_IMPL(AudioDestinationNodeHostObject, channelInterpretation) {
-    return jsi::String::createFromUtf8(
-            runtime, js_enum_parser::channelInterpretationToString(channelInterpretation_));
+  return jsi::String::createFromUtf8(
+      runtime, js_enum_parser::channelInterpretationToString(channelInterpretation_));
 }
 
 /// AudioDestinationNode is the end point of the audio graph, it cannot connect to any other node, so connect and disconnect are no-op
