3973fac8b0e71fdc5eaf2a54b95a9572e99ed507
Kacper Orszulak <42392219+SomePersonFromMars@users.noreply.github.com>
2026-07-13 16:22:27 +0200

    Docs/audiodocs cleanup (#1156)

--- packages/audiodocs/src/components/Badges/index.tsx
+++ packages/audiodocs/src/components/Badges/index.tsx
@@ -1,30 +1,30 @@
 import React from 'react';
 import styles from './styles.module.css';
 
-export function Optional({ footnote }) {
+export function Optional({ footnote }: { footnote?: boolean }) {
   return <div className={`${styles.badge} ${styles.basic}`}>Optional{footnote ? '*' : ''}</div>;
 }
 
-export function ReadOnly({ footnote }) {
+export function ReadOnly({ footnote }: { footnote?: boolean }) {
   return <div className={`${styles.badge} ${styles.basic}`}>Read only{footnote ? '*' : ''}</div>;
 }
 
-export function Overridden({ footnote }) {
+export function Overridden({ footnote }: { footnote?: boolean }) {
   return <div className={`${styles.badge} ${styles.basic}`}>Overridden{footnote ? '*' : ''}</div>;
 }
 
-export function IOS({ footnote }) {
+export function IOS({ footnote }: { footnote?: boolean }) {
   return <div className={`${styles.badge} ${styles.basic}`}>iOS{footnote ? '*' : ''}</div>;
 }
 
-export function Android({ footnote }) {
+export function Android({ footnote }: { footnote?: boolean }) {
   return <div className={`${styles.badge} ${styles.basic}`}>Android{footnote ? '*' : ''}</div>;
 }
 
-export function Experimental({ footnote }) {
+export function Experimental({ footnote }: { footnote?: boolean }) {
   return <div className={`${styles.badge} ${styles.experimental}`}>Experimental{footnote ? '*' : ''}</div>;
 }
 
-export function MobileOnly({ footnote }) {
+export function MobileOnly({ footnote }: { footnote?: boolean }) {
   return <div className={`${styles.badge} ${styles.experimental}`}>Mobile only{footnote ? '*' : ''}</div>;
 }
