4e1ebe4ffe7e4d78c9a96a6f1b2b0550f6b14ee2
Barbara Wojtarowicz <basiavj@gmail.com>
2026-08-12 12:32:50 +0200

    feat(PannerNode): added PannerTest for monno

--- packages/react-native-audio-api/common/cpp/test/src/core/effects/PannerTest.cpp
+++ packages/react-native-audio-api/common/cpp/test/src/core/effects/PannerTest.cpp
@@ -2,6 +2,7 @@
 #include <audioapi/core/OfflineAudioContext.h>
 #include <audioapi/core/destinations/AudioDestinationNode.h>
 #include <audioapi/core/effects/PannerNode.h>
+#include <audioapi/core/effects/PannerSpatialization.h>
 #include <audioapi/types/NodeOptions.h>
 #include <audioapi/utils/AudioBuffer.hpp>
 #include <gtest/gtest.h>
@@ -10,6 +11,7 @@
 #include <memory>
 
 using namespace audioapi;
+using namespace audioapi::panner;
 
 // NOLINTBEGIN
 
@@ -45,6 +47,75 @@ class TestablePannerNode : public PannerNode {
 TEST_F(PannerTest, PannerCanBeCreated) {
   auto panner = std::make_shared<PannerNode>(context, listener.get(), PannerOptions());
   ASSERT_NE(panner, nullptr);
+}
+
+TEST_F(PannerTest, PannerCanBeCreatedWithCone) {
+  auto panner = std::make_shared<PannerNode>(context, listener.get(), PannerOptions());
+  panner->setConeInnerAngle(60);
+  panner->setConeOuterAngle(90);
+  panner->setConeOuterGain(0);
+  ASSERT_NE(panner, nullptr);
+}
+
+TEST_F(PannerTest, PanModulatesInputMonoCorrectly) {
+  static constexpr int FRAMES_TO_PROCESS = 4;
+  TestablePannerNode panNode(context, listener.get());
+  listener->getPositionXParam()->setValue(1.0f);
+  listener->getPositionYParam()->setValue(0.0f);
+  listener->getPositionZParam()->setValue(0.0f);
+
+  panNode.getPositionXParam()->setValue(1.0f);
+
+  auto monoInputBuffer = std::make_shared<DSPAudioBuffer>(FRAMES_TO_PROCESS, 1, sampleRate);
+  for (size_t i = 0; i < monoInputBuffer->getSize(); ++i) {
+    (*monoInputBuffer->getChannelByType(AudioBuffer::ChannelMono))[i] = i + 1;
+  }
+
+  panNode.setInputBuffer(monoInputBuffer);
+  panNode.processNode(FRAMES_TO_PROCESS);
+
+  Vec3 sourcePosition = {1.0f, 0.0f, 0.0f};
+  Vec3 listenerPosition = {0.0f, 0.0f, 0.0f};
+  Vec3 listenerForward = {0.0f, 0.0f, -1.0f};
+  Vec3 listenerUp = {0.0f, 1.0f, 0.0f};
+
+  const auto inputLeftSpan = monoInputBuffer->getChannelByType(AudioBuffer::ChannelMono)->span();
+
+  // float azimuth = panner::computeAzimuth(sourcePosition, listenerPosition, listenerForward, listenerUp);
+  // std::cout << "Azimuth: " << azimuth << std::endl;
+  // azimuth = panner::clampAzimuth(azimuth);
+  // std::cout << "Clamped Azimuth: " << azimuth << std::endl;
+  // azimuth = panner::wrapAzimuth(azimuth);
+  // std::cout << "Wrapped Azimuth: " << azimuth << std::endl;
+
+  // float x = (azimuth + DEG_90) / DEG_180;
+  // std::cout << "X: " << x << std::endl;
+
+  // float gainL = std::cos(x * PI / 2.0f);
+  // float gainR = std::sin(x * PI / 2.0f);
+
+  // std::cout << "Gain L: " << gainL << std::endl;
+  // std::cout << "Gain R: " << gainR << std::endl;
+
+  // For such given source and listener positions, the azimuth is 90 degrees.
+  // gainL = -4.37114e-08, gainR = 1.0
+  constexpr float EXPECTED_GAIN_L_AZIMUTH_90 = -4.37114e-08;
+  constexpr float EXPECTED_GAIN_R_AZIMUTH_90 = 1.0;
+
+  auto resultBuffer = panNode.getOutputBuffer();
+  const auto outputLeftSpan = resultBuffer->getChannelByType(AudioBuffer::ChannelLeft)->span();
+  const auto outputRightSpan = resultBuffer->getChannelByType(AudioBuffer::ChannelRight)->span();
+
+  for (size_t i = 0; i < FRAMES_TO_PROCESS; ++i) {
+    EXPECT_NEAR(
+        (*resultBuffer->getChannelByType(AudioBuffer::ChannelMono))[i],
+        (i + 1) * EXPECTED_GAIN_L_AZIMUTH_90,
+        1e-4);
+    EXPECT_NEAR(
+        (*resultBuffer->getChannelByType(AudioBuffer::ChannelRight))[i],
+        (i + 1) * EXPECTED_GAIN_R_AZIMUTH_90,
+        1e-4);
+  }
 }
 
 TEST_F(PannerTest, MonoSourcePannedByPosition) {
