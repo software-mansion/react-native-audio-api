5bca10f5dcb69e9294be4265d5d2e58b4da08a07
michal <dydmichal@gmail.com>
2026-08-21 15:21:26 +0200

    fix: moved the event purely to c++

--- packages/react-native-audio-api/common/cpp/audioapi/core/types/ContextState.h
+++ packages/react-native-audio-api/common/cpp/audioapi/core/types/ContextState.h
@@ -6,4 +6,18 @@ namespace audioapi {
 
 enum class ContextState : std::uint8_t { SUSPENDED, RUNNING, CLOSED };
 
+/// The Web Audio API `AudioContextState` string for @p state, as carried by
+/// the `statechange` event payload and the JS `state` attribute.
+inline const char *contextStateToString(ContextState state) {
+  switch (state) {
+    case ContextState::SUSPENDED:
+      return "suspended";
+    case ContextState::RUNNING:
+      return "running";
+    case ContextState::CLOSED:
+      return "closed";
+  }
+  return "suspended";
 }
+
+} // namespace audioapi
