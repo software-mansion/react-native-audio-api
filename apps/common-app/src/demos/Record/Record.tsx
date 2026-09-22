9ed5884c24c9efa98b6d4d341acc0a69a777a284
Kacper Orszulak <42392219+SomePersonFromMars@users.noreply.github.com>
2026-09-22 17:15:03 +0200

    fix: remove accidental recorder debugging remainder (#1304)

--- apps/common-app/src/demos/Record/Record.tsx
+++ apps/common-app/src/demos/Record/Record.tsx
@@ -103,8 +103,6 @@ const Record: FC = () => {
     const result = await Recorder.start({
       fileNameOverride: `overridden_name_${Date.now()}`,
     });
-    const Recorder2 = new AudioRecorder();
-    Recorder2.start();
 
     setupNotification(false);
 
